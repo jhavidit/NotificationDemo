7d0f037e0fe43a9bf1178dba884e8fbfbb8d27db
jhavidit <54817177+jhavidit@users.noreply.github.com>
2020-06-03 15:30:20 +0530

    initial commit

--- app/src/main/java/com/example/notificationdemo/MainActivity.kt
+++ app/src/main/java/com/example/notificationdemo/MainActivity.kt
@@ -1,11 +1,75 @@
 package com.example.notificationdemo
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.widget.RemoteViews
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_notification.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var notificationChannel: NotificationChannel
+    private lateinit var builder: Notification.Builder
+    private var channelID = "com.example.notificationdemo"
+    private var description = "Test Notification"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notification_button.setOnClickListener {
+
+            val intent = Intent(this, NotificationScreen::class.java)
+            val pendingIntent =
+                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            val contentView=RemoteViews(packageName,R.layout.custom_notification)
+            contentView.setTextViewText(R.id.not_title,"Hunter")
+            contentView.setTextViewText(R.id.not_content,"Notification Demo")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationChannel =
+                    NotificationChannel(channelID, description, NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.enableLights(true)
+                notificationChannel.lightColor = Color.GREEN
+                notificationChannel.enableVibration(false)
+                notificationManager.createNotificationChannel(notificationChannel)
+
+                builder=Notification.Builder(this,channelID)
+                    .setContent(contentView)
+                    .setContentTitle("Notification Demo")
+                    .setContentText("Test Notification")
+                    .setSmallIcon(R.drawable.ic_launcher_round)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.drawable.ic_launcher))
+                    .setContentIntent(pendingIntent)
+            }
+            else
+            {
+                builder=Notification.Builder(this)
+                    .setContent(contentView)
+                    .setContentTitle("Notification Demo")
+                    .setContentText("Test Notification")
+                    .setSmallIcon(R.drawable.ic_launcher_round)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.drawable.ic_launcher))
+                    .setContentIntent(pendingIntent)
+            }
+            notificationManager.notify(1234,builder.build())
+
+
+
+
+        }
     }
 }
